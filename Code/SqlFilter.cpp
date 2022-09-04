29f76d5222339bc2d6941de97716c6cd5fa0d66f
Vladyslav Slovinskyi <msc.slowinski@gmail.com>
2022-09-04 23:11:25 +0200

    Update SQL filter

--- Code/SqlFilter.cpp
+++ Code/SqlFilter.cpp
@@ -1,6 +1,17 @@
 #include "SqlFilter.h"
 
+#include <QDate>
+
 QString filterRecordsByPeriod(const QString& table, const PeriodType& type)
 {
+    switch (type) {
+    case PeriodType::ALL:
+        return QString{"SELECT * FROM "} + table;
+    case PeriodType::CURRENT_DAY:
+        return QString{"SELECT * FROM "} + table + QString{" WHERE date = '"} +
+            QString{QDate::currentDate().toString("yyyy-MM-dd")} + QString{"'"};
+    default:
+        return QString{"SELECT * FROM expenses"};
+    }
 
 }
