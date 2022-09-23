d25d34d8c7081166888660a150ebe4f8585d11a9
Vladyslav Slovinskyi <msc.slowinski@gmail.com>
2022-09-23 21:58:49 +0200

    Update Main Widget setting

--- Widget/MainWidget.cpp
+++ Widget/MainWidget.cpp
@@ -6,10 +6,19 @@ MainWidget::MainWidget(QWidget *parent) :
     ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
+    QStringList list = QStringList() << "PL" << "EUR" << "USD";
+    ui->cbbSymbol->addItems(list);
     ui->dtExchangeRateDate->setDate(QDate::currentDate());
+    ui->dtExchangeRateDate->setEnabled(false);
+    connect(ui->cbArchivedExchangeRates, &QCheckBox::clicked, this, &MainWidget::setting);
 }
 
 MainWidget::~MainWidget()
 {
     delete ui;
+}
+
+void MainWidget::setting()
+{
+    ui->dtExchangeRateDate->setEnabled(ui->cbArchivedExchangeRates->isChecked());
 }
--- Widget/MainWidget.h
+++ Widget/MainWidget.h
@@ -17,6 +17,9 @@ public:
 
 private:
     Ui::MainWidget *ui;
+
+private slots:
+    void setting();
 };
 
 #endif
